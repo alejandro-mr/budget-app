31e3080141fa94b2daa9f485b4ed9cc8a84cde71
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2018-07-23 04:48:03 -0700

    Adding dockerfile for client app and new webpack configuration.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,43 +5,88 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const autoprefixer = require('autoprefixer');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-var isProd = (process.env.NODE_ENV === 'production');
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 
 module.exports = {
   entry: './src/app.js',
-  devtool: 'inline-source-map',
-  mode: 'development',
+  devtool: shouldUseSourceMap ? 'source-map' : false,
+  mode: 'production',
   devServer: {
     contentBase: './dist',
     stats: 'minimal',
     host: '0.0.0.0',
-    port: 8000,
+    port: 4200,
     hot: true,
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          parse: {
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+        },
+        parallel: true,
+        cache: true,
+        sourceMap: shouldUseSourceMap,
+      }),
+      new OptimizeCSSAssetsPlugin({ cssProcessorOptions: { safe: true } }),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      name: 'vendors',
+    },
+    runtimeChunk: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: [/[/\\\\]node_modules[/\\\\]/],
         loader: 'babel-loader'
       },
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'postcss-loader'
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: shouldUseSourceMap,
+            },
+          },
         ]
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-        exclude: /node_modules/,
+        exclude:  [/[/\\\\]node_modules[/\\\\]/],
         loader: 'file-loader'
       },
       {
         test: /\.html$/,
-        exclude: /node_modules/,
+        exclude:  [/[/\\\\]node_modules[/\\\\]/],
         loader: 'raw-loader'
       }
     ]
@@ -64,7 +109,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/src/assets',
+    publicPath: '/',
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
   }
