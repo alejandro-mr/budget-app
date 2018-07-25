8083dc65a2b58aaecb94a5927f5f58df481ad1fb
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2018-07-24 22:56:58 -0700

    Using mysql-client to check if host is ready to accept connections.

--- api/docker/entrypoint/docker-entypoint.sh
+++ api/docker/entrypoint/docker-entypoint.sh
@@ -2,6 +2,9 @@
 set -e
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
+    while ! mysqladmin ping -h"$DB_HOST" --silent; do
+        sleep 1
+    done
     bin/console doctrine:migrations:migrate --no-interaction
 fi
 
