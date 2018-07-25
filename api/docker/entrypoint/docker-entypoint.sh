0e01eff980f22ef69e19602496fe3c364d812acd
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2018-07-25 05:09:20 -0700

    Adding changes to entrypoint file to fix issues with image creation.

--- api/docker/entrypoint/docker-entypoint.sh
+++ api/docker/entrypoint/docker-entypoint.sh
@@ -2,11 +2,15 @@
 set -e
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-    >&2 echo "Waiting for MariaDB to be ready..."
-    while ! mysqladmin ping -h"$DB_HOST" --silent; do
-        sleep 1
-    done
-    bin/console doctrine:migrations:migrate --no-interaction
+    if [ "$APP_ENV" != 'prod' ]; then
+        composer install --prefer-dist --no-progress --no-suggest --no-interaction
+
+        >&2 echo "Waiting for MariaDB to be ready..."
+        while ! mysqladmin ping -h"$DB_HOST" --silent; do
+            sleep 1
+        done
+        bin/console doctrine:migrations:migrate --no-interaction
+    fi
 fi
 
 exec docker-php-entrypoint "$@"
