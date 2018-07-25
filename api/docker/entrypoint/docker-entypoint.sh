cc5900c76a6df4433b34f9cc362279022a880031
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2018-07-25 01:29:22 -0700

    Fixing issues when creating php-fpm based image and entrypoint.

--- api/docker/entrypoint/docker-entypoint.sh
+++ api/docker/entrypoint/docker-entypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
+    >&2 echo "Waiting for MariaDB to be ready..."
     while ! mysqladmin ping -h"$DB_HOST" --silent; do
         sleep 1
     done
